834f981f608975692e664072a8c7d9a44bdb3920
Josh Swan <joshuawswan@gmail.com>
2018-04-08 17:07:26 -0700

    Add tests

---
+++ utils/createComponentWithGlobalize.js
@@ -0,0 +1,19 @@
+/*!
+ * React Native Globalize
+ *
+ * Copyright 2015-2018 Josh Swan
+ * Released under the MIT license
+ * https://github.com/joshswan/react-native-globalize/blob/master/LICENSE
+ */
+
+import React from 'react';
+import renderer from 'react-test-renderer'; // eslint-disable-line import/no-extraneous-dependencies
+import FormattedProvider from '../lib/components/FormattedProvider';
+
+export default function createComponentWithGlobalize(children, props = { currency: 'USD', locale: 'en' }) {
+  return renderer.create((
+    <FormattedProvider {...props}>
+      {children}
+    </FormattedProvider>
+  ));
+}
