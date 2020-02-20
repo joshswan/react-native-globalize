0b6a02ce1dd441e775c659ae3e55861b4f0ed5f9
Josh Swan <joshuawswan@gmail.com>
2020-02-19 20:55:29 -0800

    chore: configure commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*!
+ * React Native Globalize
+ *
+ * Copyright 2015-2020 Josh Swan
+ * Released under the MIT license
+ * https://github.com/joshswan/react-native-globalize/blob/master/LICENSE
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
